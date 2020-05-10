75b08f42d02af2f12a675f597e6d30f040bf9a4e
Nithin Raj <nithin.raj@iiitb.org>
2020-05-11 00:11:31 +0530

    Added Beta Codeword Dictionary

--- app/src/main/java/com/example/firstapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/firstapp/DisplayMessageActivity.kt
@@ -15,6 +15,7 @@ class DisplayMessageActivity : AppCompatActivity(), TextToSpeech.OnInitListener 
     private var tts: TextToSpeech? = null
     private var buttonSpeak: Button? = null
     private var textSpeak: String? = null
+    private var textToSend:String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,22 +26,30 @@ class DisplayMessageActivity : AppCompatActivity(), TextToSpeech.OnInitListener 
         buttonSpeak!!.setOnClickListener { speakOut() }
         val message = intent.getStringExtra(EXTRA_MESSAGE)
         textSpeak = message
+        when (textSpeak) {
+            "1001" -> textToSend = "Codeword Sequence 1,0,0,1 activated"
+            "1011" -> textToSend = "Codeword Sequence 1,0,1,1 activated"
+            else -> { // Note the block
+                textToSend = "Tame Impala is love"
+            }
+        }
+        textSpeak = textToSend
         val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
+            text = textToSend
         }
     }
 
     override fun onBackPressed() {
         val intent = Intent(this@DisplayMessageActivity, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        intent.putExtra("SendToQueue", textSpeak)
+        intent.putExtra("SendToQueue", textToSend)
         startActivity(intent)
     }
 
     override fun onSupportNavigateUp():Boolean {
         val intent = Intent(this@DisplayMessageActivity, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        intent.putExtra("SendToQueue", textSpeak)
+        intent.putExtra("SendToQueue", textToSend)
         startActivity(intent)
         return true
     }
